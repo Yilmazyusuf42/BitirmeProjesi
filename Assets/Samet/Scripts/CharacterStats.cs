7370dc54f4b809965e2631f89f70bddb8a7d7fe8
Yuichiroo <sametmc7466@gmail.com>
2025-05-11 00:52:51 +0300

    Update CharacterStats.cs

--- Assets/Samet/Scripts/CharacterStats.cs
+++ Assets/Samet/Scripts/CharacterStats.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strength;
-    public Stat damage;
+
+
+    [Header("Major Stats")]
+    public Stat strength; //1 point increase damage by 1 and crit power by %1
+    public Stat agility;  // 1 point increase evasion by %1 and crit chance by %1
+    public Stat intelligence; // 1 point increase magick damage by 1 and magic resistance by 3
+    public Stat vitality; // 1 point increase health by 3 or 5 points
+
+
+    [Header("Defensive Stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+
+    public Stat damage;
 
 
 
@@ -22,6 +35,13 @@ public class CharacterStats : MonoBehaviour
     
     public virtual void DoDamage(CharacterStats _targerStats)
     {
+        int totalEvasion= _targerStats.evasion.GetValue()+_targerStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            Debug.Log("Attack Avoided");
+            return;
+        }
 
         Debug.Log("Uygulandý");
 
